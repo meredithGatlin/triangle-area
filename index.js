e89bc430f24bbfcaca24d4dfab7a50c169ad0749
meredithGatlin <mgarcia91@student.cccd.edu>
2024-03-07 11:06:42 -0800

    answer and passes test!

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Calculates the area of a triangle based on the given base and the given height.
 // The area A of a triangle is given by the formula A = BxH/2 where B is the base
 // and H is the height of the triangle.
+
 function getTriangleArea(base, height) {
   // TODO: Implement this function.
-  const result = -1;
+  const area = (base * height)/2
+  const result = area;
   return result;
 }
 
